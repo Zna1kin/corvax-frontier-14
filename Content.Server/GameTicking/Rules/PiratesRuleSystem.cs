26596148f222575b514716b9ecfc17f9e36c6fac
Zekins <136648667+Zekins3366@users.noreply.github.com>
2024-04-22 21:37:29 +0300

    Update PiratesRuleSystem.cs

--- Content.Server/GameTicking/Rules/PiratesRuleSystem.cs
+++ Content.Server/GameTicking/Rules/PiratesRuleSystem.cs
@@ -518,7 +518,7 @@ public sealed class PiratesRuleSystem : GameRuleSystem<PiratesRuleComponent>
 
         if (spawns.Count == 0)
         {
-            spawns.Add(Transform(.Coordinates));
+            spawns.Add(Transform(component.DefaultCoordinates));
             Logger.WarningS("nukies", $"Fell back to default spawn for nukies!");
         }
 
